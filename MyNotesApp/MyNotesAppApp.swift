d4a12cae3af81e4ed05da84e41617f55da77ae7b
Harshul <harshulnarang@ymail.com>
2026-07-14 20:49:27 +0100

    Initial Commit

---
+++ MyNotesApp/MyNotesAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotesAppApp.swift
+//  MyNotesApp
+//
+//  Created by Harshul on 14/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MyNotesAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
